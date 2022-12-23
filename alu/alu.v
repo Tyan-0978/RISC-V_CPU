74493151ea664d0bd32e0a6f40f06110e85ade0f
Tyan-0978 <960007nathan@gmail.com>
2022-12-24 02:43:25 +0800

    initial commit

---
+++ alu/alu.v
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------------
+// ALU top module
+// -----------------------------------------------------------------------------
+
+module alu (
+    input  i_rst_n,
+    input  i_clk,
+    input  [?:0]  i_mode, // TODO
+    input  [31:0] i_a,
+    input  [31:0] i_b,
+    output [31:0] o_result,
+    output o_stall
+);
+
+// registers & wires -------------------------------------------------
+// sub-module ports
+
+// control signals
+
+// sub-modules -------------------------------------------------------
+// TODO
+
+endmodule
