20fa4b71e19a04a5a0edce0f228fab3d1e047038
Tyan-0978 <960007nathan@gmail.com>
2022-12-23 16:37:56 +0800

    fix replication error; test passed

--- alu/int_add_sub.v
+++ alu/int_add_sub.v
@@ -4,15 +4,15 @@
 
 module int_add_sub (
     input  i_mode, // 0: add, 1: subtract
-    input  signed [31:0] i_a,
-    input  signed [31:0] i_b,
-    output signed [31:0] o_result
+    input  [31:0] i_a,
+    input  [31:0] i_b,
+    output [31:0] o_result
 );
 
-wire signed [31:0] op_b;
-wire signed [31:0] out;
+wire [31:0] op_b;
+wire [31:0] out;
 
-assign op_b = i_b ^ i_mode;
+assign op_b = i_b ^ {32{i_mode}};
 assign out = i_a + op_b + i_mode;
 assign o_result = out;
 
