de86b5e8286b9aadd2c5ed4a10cb1e208548ca52
Tyan-0978 <960007nathan@gmail.com>
2022-12-23 22:49:50 +0800

    fix replication error; test passed

--- alu/int_mul.v
+++ alu/int_mul.v
@@ -21,8 +21,8 @@ parameter DONE = 2;
 
 // control signals
 reg  [1:0] state, next_state;
-reg        valid, next_valid;
 reg  [4:0] count, next_count;
+wire last_cycle;
 
 // computation signals
 wire out_sign;
@@ -32,16 +32,16 @@ reg  [61:0] shift_reg, next_shift_reg; // 31 + 31 bits
 reg  [31:0] result, next_result;
 
 // wire assignments --------------------------------------------------
-
 assign out_sign = i_a[31] ^ i_b[31];
+assign last_cycle = (count == 31);
 
 // adder
-assign add_in_a = i_a[30:0] & {{shift_reg[0]}};
+assign add_in_a = i_a[30:0] & {31{shift_reg[0]}};
 assign add_in_b = shift_reg[61:31];
 assign add_out = add_in_a + add_in_b;
 
 // output
-assign o_valid = valid;
+assign o_valid = (state == DONE);
 assign o_result = result;
 
 // combinational always block ----------------------------------------
@@ -55,19 +55,15 @@ always @(*) begin
 	    else begin
 	        next_state = state;
 	    end
-	    next_valid = 0;
 	    next_count = 0;
 	end
 	CALC: begin
-	    if (count == 30) begin
+	    if (last_cycle) begin
 	        next_state = DONE;
-	        next_valid = 1;
 	    end
 	    else begin
 	        next_state = state;
-	        next_valid = 0;
 	    end
-	    next_valid = 0;
 	    next_count = count + 1;
 	end
 	DONE: begin
@@ -77,12 +73,10 @@ always @(*) begin
 	    else begin
 	        next_state = state;
 	    end
-	    next_valid = 1;
 	    next_count = 0;
 	end
 	default: begin
 	    next_state = IDLE;
-	    next_valid = 0;
 	    next_count = 0;
 	end
     endcase
@@ -102,11 +96,11 @@ always @(*) begin
     end
 
     // next output
-    if (state == DONE) begin
+    if (last_cycle) begin
         next_result = {out_sign, shift_reg[30:0]};
     end
     else begin
-        next_result = 0;
+        next_result = result;
     end
 end
 
@@ -114,14 +108,12 @@ end
 always @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
 	state <= IDLE;
-        valid <= 0;
 	count <= 0;
 	result <= 0;
 	shift_reg <= 0;
     end
     else begin
 	state <= next_state;
-        valid <= next_valid;
 	count <= next_count;
 	result <= next_result;
 	shift_reg <= next_shift_reg;
