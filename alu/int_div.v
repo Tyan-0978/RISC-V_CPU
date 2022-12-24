9048e3d4dcc580a757a1819b8b44e86a69bbab5a
Tyan-0978 <960007nathan@gmail.com>
2022-12-24 15:16:25 +0800

    test with positive operands passed; definition of operation on negative operands TBD

--- alu/int_div.v
+++ alu/int_div.v
@@ -19,39 +19,44 @@ parameter CALC = 1;
 parameter DONE = 2;
 
 // wires & registers -------------------------------------------------
-
 // control signals
 reg  [1:0] state, next_state;
 reg  [4:0] count, next_count;
+wire last_cycle;
 
 // computation signals
+wire        [30:0] val_a, val_b;
 wire        [31:0] sub_in_a, sub_in_b;
 wire signed [32:0] sub_out; // 33 bits
 reg         [61:0] shift_reg, next_shift_reg; // 31 + 31 bits
 
 wire        reverse_quotient;
-wire        remainder_mode;
+wire [ 1:0] remainder_mode;
 wire [31:0] comp_remainder; // complement
 
 // output
-reg         valid, next_valid;
 reg  [31:0] quotient , next_quotient;
 reg  [31:0] remainder, next_remainder;
 
 // wire assignments --------------------------------------------------
+assign last_cycle = (count == 31);
+
+// input values
+assign val_a = (i_a[31]) ? (~i_a[30:0]) + 1 : i_a[30:0];
+assign val_b = (i_b[31]) ? (~i_b[30:0]) + 1 : i_b[30:0];
 
 // subtractor
 assign sub_in_a = shift_reg[61:30];
-assign sub_in_b = {1'b0, i_b[30:0]};
+assign sub_in_b = {1'b0, val_b};
 assign sub_out = sub_in_a - sub_in_b;
 
 // results
 assign reverse_quotient = i_a[31] ^ i_b[31];
 assign remainder_mode = {i_a[31], i_b[31]};
-assign comp_remainder = i_b[30:0] - shift_reg[61:31];
+assign comp_remainder = val_b - shift_reg[61:31];
 
 // output
-assign o_valid = valid;
+assign o_valid = (state == DONE);
 assign o_quotient = quotient;
 assign o_remainder = remainder;
 
@@ -67,19 +72,15 @@ always @(*) begin
 	    else begin
 	        next_state = state;
 	    end
-	    next_valid = 0;
 	    next_count = 0;
 	end
 	CALC: begin
-	    if (count == 30) begin
+	    if (last_cycle) begin
 	        next_state = DONE;
-	        next_valid = 1;
 	    end
 	    else begin
 	        next_state = state;
-	        next_valid = 0;
 	    end
-	    next_valid = 0;
 	    next_count = count + 1;
 	end
 	DONE: begin
@@ -89,19 +90,17 @@ always @(*) begin
 	    else begin
 	        next_state = state;
 	    end
-	    next_valid = 1;
 	    next_count = 0;
 	end
 	default: begin
 	    next_state = IDLE;
-	    next_valid = 0;
 	    next_count = 0;
 	end
     endcase
 
     // next shift register
     if (i_valid) begin
-        next_shift_reg = {31'd0, i_a[30:0]};
+        next_shift_reg = {31'd0, val_a};
     end
     else begin
         if (state == CALC) begin
@@ -120,13 +119,13 @@ always @(*) begin
     end
 
     // next output
-    if (state == DONE) begin
+    if (last_cycle) begin
         // example of results for different sign mode
         //  7 /  3 =  2 ...  1
         //  7 / -3 = -3 ... -2
         // -7 /  3 = -3 ...  2
         // -7 / -3 =  2 ... -1
-        next_quotient  = {{reverse_quotient}} ^ {1'b0, shift_reg[30:0]};
+        next_quotient  = {32{reverse_quotient}} ^ {1'b0, shift_reg[30:0]};
         case (remainder_mode)
             2'b00: next_remainder = shift_reg[61:31];
             2'b01: next_remainder = (~comp_remainder) + 1;
@@ -135,8 +134,8 @@ always @(*) begin
         endcase
     end
     else begin
-        next_quotient  = 0;
-        next_remainder = 0;
+        next_quotient  = quotient;
+        next_remainder = remainder;
     end
 end
 
@@ -145,7 +144,6 @@ end
 always @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
 	state <= IDLE;
-        valid <= 0;
 	count <= 0;
 	quotient  <= 0;
 	remainder <= 0;
@@ -153,7 +151,6 @@ always @(posedge i_clk or negedge i_rst_n) begin
     end
     else begin
 	state <= next_state;
-        valid <= next_valid;
 	count <= next_count;
 	quotient  <= next_quotient;
 	remainder <= next_remainder;
