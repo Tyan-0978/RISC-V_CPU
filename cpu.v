739af8aa00f4ac6849fa107fd58c43f88eee9571
Tyan-0978 <960007nathan@gmail.com>
2022-12-25 20:53:00 +0800

    finish signal propagation through pipeline stages

--- cpu.v
+++ cpu.v
@@ -21,7 +21,7 @@ wire [31:0] id_i_inst_data;
 reg  [4:0]  id_o_rd, id_o_rs1, id_o_rs2, id_rd, id_rs1, id_rs2;
 reg  [31:0] id_imm, id_o_imm;
 reg  [2:0]  id_funct3, id_o_funct3;
-reg  id_alusrc, id_mem_treg, id_reg_write;
+reg  id_alusrc, id_mem_to_reg, id_reg_write;
 reg  id_o_alusrc, id_o_mem_to_reg, id_o_reg_write;
 reg  id_mem_read, id_mem_write, id_branch;
 reg  id_o_mem_read, id_o_mem_write, id_o_branch;
@@ -29,6 +29,20 @@ reg  [2:0] id_op_mode, id_func_op, id_o_op_mode, id_o_func_op;
 reg  id_fp_mode, id_o_fp_mode;
 
 // register file stage
+wire rf_i_reg_write,
+wire [ 4:0] rf_i_write_rd;
+wire [31:0] rf_i_write_data;
+wire [ 4:0] rf_i_read_rs1;
+wire [ 4:0] rf_i_read_rs2;
+wire [31:0] rf_o_rs1_data;
+wire [31:0] rf_o_rs2_data;
+
+reg  [31:0] rf_imm;
+reg  [2:0]  rf_funct3;
+reg  rf_alusrc, rf_mem_to_reg, rf_reg_write;
+reg  rf_mem_read, rf_mem_write, rf_branch;
+reg  [2:0] rf_op_mode, rf_func_op;
+reg  rf_fp_mode;
 
 // ALU stage
 wire [2:0] alu_i_op_mode;
@@ -36,8 +50,18 @@ wire [2:0] alu_i_func_op;
 wire alu_i_fp_mode;
 wire alu_i_stall, alu_o_stall;
 wire [31:0] alu_i_a, alu_i_b, alu_o_result;
+// TODO: branch result
+
+reg  [2:0] alu_funct3;
+reg  alu_mem_to_reg, alu_reg_write;
+reg  alu_mem_read, alu_mem_write;
 
 // data memory stage
+
+
+reg  dmm_mem_to_reg, dmm_reg_write;
+
+// write back stage
 
 // -------------------------------------------------------------------
 // CPU top control
@@ -83,7 +107,7 @@ inst_dec inst_dec0 (
 );
 always @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
-        id_rd <= 0; 
+        id_rd <= 0;
         id_rs1 <= 0;
         id_rs2 <= 0;
         id_imm <= 0;
@@ -118,12 +142,58 @@ end
 // -------------------------------------------------------------------
 // register file stage 
 // -------------------------------------------------------------------
-// TODO
+assign rf_i_reg_write = id_reg_write;
+assign rf_i_write_rd = // TODO: connect from write back stage
+assign rf_i_write_data = // TODO: connect from write back stage
+assign i_read_rs1 = id_rs1;
+assign i_read_rs2 = id_rs2;
+
+reg_file reg_file0 (
+    .i_rst_n(i_rst_n), .i_clk(i_clk),
+    .i_reg_write(rf_i_reg_write),
+    .i_write_rd(rf_i_write_rd), .i_write_data(rf_i_write_data),
+    .i_read_rs1(rf_i_read_rs1), .i_read_rs2(rf_i_read_rs2),
+    .o_rs1_data(rf_o_rs1_data), .o_rs2_data(rf_o_rs2_data)
+);
+
+always @(posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+        rf_imm <= 0;
+        rf_funct3 <= 0;
+        rf_alusrc <= 0;
+        rf_mem_to_reg <= 0;
+        rf_reg_write <= 0;
+        rf_mem_read <= 0;
+        rf_mem_write <= 0;
+        rf_branch <= 0;
+        rf_op_mode <= 0;
+        rf_func_op <= 0;
+        rf_fp_mode <= 0;
+    end else begin
+        rf_imm <= id_imm;
+        rf_funct3 <= id_funct3;
+        rf_alusrc <= id_alusrc;
+        rf_mem_to_reg <= id_mem_to_reg;
+        rf_reg_write <= id_reg_write;
+        rf_mem_read <= id_mem_read;
+        rf_mem_write <= id_mem_write;
+        rf_branch <= id_branch;
+        rf_op_mode <= id_op_mode;
+        rf_func_op <= id_func_op;
+        rf_fp_mode <= id_fp_mode;
+    end
+end
 
 // -------------------------------------------------------------------
 // ALU stage 
 // -------------------------------------------------------------------
-// TODO: ALU control
+assign alu_i_op_mode = rf_op_mode;
+assign alu_i_func_op = rf_func_op;
+assign alu_i_fp_mode = rf_fp_mode;
+assign alu_i_stall = // TODO
+assign alu_i_a = rf_o_rs1_data;
+assign alu_i_b = (rf_alusrc) ? rf_imm : rf_o_rs2_data;
+
 alu alu0 (
     .i_rst_n(i_rst_n), .i_clk(i_clk),
     .i_op_mode(alu_i_op_mode), .i_func_op(alu_i_func_op), 
@@ -133,8 +203,38 @@ alu alu0 (
     .o_stall(alu_o_stall)
 );
 
+always @(posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+        alu_funct3 <= 0;
+        alu_mem_to_reg <= 0;
+        alu_reg_write <= 0;
+        alu_mem_read <= 0;
+        alu_mem_write <= 0;
+    end else begin
+        alu_funct3 <= rf_funct3;
+        alu_mem_to_reg <= rf_mem_to_reg;
+        alu_reg_write <= rf_reg_write;
+        alu_mem_read <= rf_mem_read;
+        alu_mem_write <= rf_mem_write;
+    end
+end
+
 // -------------------------------------------------------------------
 // data memory stage
+// -------------------------------------------------------------------
+// TODO
+always @(posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+        dmm_mem_to_reg <= 0;
+        dmm_reg_write <= 0;
+    end else begin
+        dmm_mem_to_reg <= alu_mem_to_reg;
+        dmm_reg_write <= alu_reg_write;
+    end
+end
+
+// -------------------------------------------------------------------
+// write back stage
 // -------------------------------------------------------------------
 // TODO
 
