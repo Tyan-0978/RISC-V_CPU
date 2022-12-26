b594c33aa1ea0b2b1b14025d51f232b0a6d48250
Tyan-0978 <960007nathan@gmail.com>
2022-12-26 17:38:43 +0800

    new

--- tb_cpu/tb_cpu.sv
+++ tb_cpu/tb_cpu.sv
@@ -16,6 +16,8 @@ always #HCLK clk = ~clk;
 
 logic start;
 logic [31:0] inst;
+logic out_ecall;
+logic out_ecall_data;
 
 int f_inst;
 logic [31:0] scan_inst;
@@ -23,8 +25,8 @@ logic [31:0] scan_inst;
 cpu cpu0 (
     .i_rst_n(rst_n), .i_clk(clk), .i_start(start),
     .i_inst(inst),
-    .o_ecall_ready(),
-    .o_ecall_data()
+    .o_ecall_ready(out_ecall),
+    .o_ecall_data(out_ecall_data)
 );
 
 initial begin
@@ -39,7 +41,7 @@ initial begin
     f_inst = $fopen("instructions.txt", "r");
     #(5*CLK)
     rst_n = 1;
-    #(5*CLK)
+    #(20*CLK)
 
     $display("======================================================================");
     $display("Start CPU test");
@@ -48,9 +50,11 @@ initial begin
     @(negedge clk)
     start <= 1;
     while (!$feof(f_inst)) begin
-        $fscanf(f_inst, "%h", scan_inst);
 	@(negedge clk)
-	inst <= scan_inst;
+        if (!cpu0.nop) begin
+            $fscanf(f_inst, "%h", scan_inst);
+	    inst <= scan_inst;
+	end
     end
 
     $fclose(f_inst);
@@ -65,5 +69,12 @@ initial begin
     $finish;
 end
 
+initial begin
+    #(100*CLK)
+    $display("======================================================================");
+    $display("Too slow, abort");
+    $display("======================================================================");
+    $finish;
+end
 
 endmodule
